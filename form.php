94dc8a7733975f8cf4c0c9bf8802dce8d150ee44
vinayak.khadye <vinayak.khadye@ril.com>
2020-05-19 19:41:27 +0530

    initial commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Jio Platforms Limited">
+        <meta name="author" content="Jio Platforms Limited">
+        <meta name="keywords" content="Jio Platforms Limited">
+
+        <title>Campaign Delivery Validation</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </body>
+</html>
